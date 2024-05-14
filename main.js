f53baddec8268ba5edd01bd9172f23850efe45a3
kiganyamburu <mburukiganya@gmail.com>
2024-05-14 12:53:40 +0300

    done

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // Give me the sum of the prices(try chaining methods)
 // [{ price: 10.99 }, { price: 5.99 }, { price: 29.99 }]
+
+const prices = [{ price: 10.99 }, { price: 5.99 }, { price: 29.99 }];
+
+const totalPrice = prices
+  .map((item) => item.price)
+  .reduce((acc, curr) => acc + curr, 0);
+
+console.log(totalPrice);
